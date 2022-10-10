5c1b3d629d957c3e41a53f96fa5430327a560149
akiya <akiya0524@gmail.com>
2022-10-10 18:12:35 +0900

    Initial Commit

---
+++ TwitterTabUITests/TwitterTabUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TwitterTabUITestsLaunchTests.swift
+//  TwitterTabUITests
+//
+//  Created by Akiya Ozawa on R 4/10/10.
+//
+
+import XCTest
+
+final class TwitterTabUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
